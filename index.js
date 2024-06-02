31220406bfa44f42176bba8944dab472c29900dd
Anirban-02 <99980141+Anirban-02@users.noreply.github.com>
2024-06-02 18:56:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { spawn } = require('child_process');
 const app = express()
-const port=process.env.PORT||5000;
+const port=process.env.PORT||4000;
 const cors=require('cors');
 const path = require('path');
 require('dotenv').config()
